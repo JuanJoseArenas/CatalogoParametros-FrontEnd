1a7ec2e636e8843154a00b69a0c0727575a4c33f
Juan Jose Arenas <juanjoseaq99@gmail.com>
2026-09-18 11:58:40 -0500

    ci: exigir cobertura minima del 80 por ciento

--- catalogo-parametros-front/karma.conf.js
+++ catalogo-parametros-front/karma.conf.js
@@ -22,7 +22,15 @@ module.exports = function (config) {
         { type: 'lcovonly' },
         { type: 'cobertura', file: 'cobertura-coverage.xml' },
         { type: 'text-summary' }
-      ]
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
     },
     junitReporter: {
       outputDir: 'test-results',
